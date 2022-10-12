4576a8fb63543797adeafbdaf08afcd683fcbbed
Chad Fernandez <chadfernandez@duck.com>
2022-10-12 23:43:11 +0800

    remove old excel extraction system, replaced by new

--- main.py
+++ main.py
@@ -1,20 +1,8 @@
 import asyncio
 import aiofiles
 
-from excel_extraction.components.get_data import GetData
-from excel_extraction.components.get_information import GetInformation
-
 if __name__ == "__main__":
 
-    async def runner():
-        df = await GetData.get_excel_data("./test/assets/PDS-Lucy-2021.xlsx")
-        json = await GetData.convert_excel_to_json(df)
+    # async def runner():
 
-        # # use aiofiles to save json to file
-        async with aiofiles.open("./test/assets/PDS-Lucy-2021.json", "w+") as f:
-            await f.write(json)
-
-        clean_data = await GetInformation.get_specific_information(json)
-        print(clean_data)
-
-    asyncio.run(runner())
+    # asyncio.run(runner())
